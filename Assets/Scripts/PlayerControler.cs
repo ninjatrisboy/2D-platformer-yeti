f372d8f209474db1a57ceaef7b9c609bc602d41b
Tristan Newball <ninjatrisboy@gmail.com>
2020-01-28 20:37:07 -0600

    Tris first repo for Unity

---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+
+    public float moveSpeed;
+    private Rigidbody2D myRigidbody;
+
+    public float jumpSpeed;
+
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+    public bool isGrounded;
+
+    private Animator myAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        myRigidbody = GetComponent<Rigidbody2D>();
+        myAnim = GetComponent<Animator>();
+    }   
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+        if (Input.GetAxisRaw("Horizontal") > 0f) 
+        {
+            myRigidbody.velocity = new Vector3(moveSpeed, myRigidbody.velocity.y, 0f);
+            transform.localScale = new Vector3(1f, 1f, 1f); 
+        }
+        else if (Input.GetAxisRaw("Horizontal") < 0f)
+        {
+            myRigidbody.velocity = new Vector3(-moveSpeed, myRigidbody.velocity.y, 0f);
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        } else {
+            myRigidbody.velocity = new Vector3(0f, myRigidbody.velocity.y, 0f);
+
+        }
+
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            myRigidbody.velocity = new Vector3(myRigidbody.velocity.x, jumpSpeed, 0f);  
+
+        }
+
+        myAnim.SetFloat("Speed", Mathf.Abs(myRigidbody.velocity.x));
+        myAnim.SetBool("Grounded", isGrounded);
+    }
+
+
+    void onTriggerEnter2D(Collider2D other)
+    {
+
+        if(other.tag == "KillPlane")
+        {
+
+            gameObject.SetActive(false);
+        }
+    }
+}
+
