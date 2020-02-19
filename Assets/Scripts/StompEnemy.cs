eaebada8b0e2fe4989a01e4f310c31502da56d34
Tristan Newball <ninjatrisboy@gmail.com>
2020-02-18 19:36:42 -0600

    Destroy enemy and wiggle walk

---
+++ Assets/Scripts/StompEnemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StompEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+     void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Enemy")
+        {
+            Destroy(other.gameObject);
+        }
+    }  
+}
