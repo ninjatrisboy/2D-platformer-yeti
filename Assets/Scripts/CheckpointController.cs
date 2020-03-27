7e6d20d7017bba42b830a37ae13d44a59bae9806
Tristan Newball <ninjatrisboy@gmail.com>
2020-03-27 09:29:49 -0500

    level music and coin sound added

---
+++ Assets/Scripts/CheckpointController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckpointController : MonoBehaviour
+{
+
+    public Sprite flagClosed;                                      
+    public Sprite flagOpen;
+
+    private SpriteRenderer theSpriteRenderer;
+
+    public bool checkpointActive;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theSpriteRenderer = GetComponent<SpriteRenderer>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Player")
+        {
+            theSpriteRenderer.sprite = flagOpen;
+            checkpointActive = true;
+        }
+    }
+}
